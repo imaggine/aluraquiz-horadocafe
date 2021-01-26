3028dc1a22ddbf2d3e71f35dd2a7f9b61006ebb2
Marcos Guiimarães <marcos@imaggineapps.com>
2021-01-26 01:04:16 -0300

    Código ao final da primeira aula de React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
+import { delBasePath } from 'next/dist/next-server/lib/router/router'
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>Meu Quiz</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+              <h1>A hora do café</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Testes de conhecimento da nossa bebida favorita</p>
+          </Widget.Content>
+
+
+        </Widget>
+        
+        <Widget>
+         <Widget.Content>
+
+            <h1>Quizes Relacionados</h1>
+            <p>- A hora do chá</p>
+
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/imaggine" />
+    </QuizBackground>
+  )
 }
